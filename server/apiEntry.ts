69bcea2ad4ea0a4c18ea41ca0935c6e3edcfed64
wahedelfar <damietta4@gmail.com>
2026-09-25 10:07:49 +0300

    chore: remove legacy Manus runtime and storage route

--- server/apiEntry.ts
+++ server/apiEntry.ts
@@ -1,4 +1,3 @@
 export { appRouter } from "./routers";
 export { createContext } from "./_core/context";
 export { registerOAuthRoutes } from "./_core/oauth";
-export { registerStorageProxy } from "./_core/storageProxy";
