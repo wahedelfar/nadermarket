8b1bc2ab84c9243eedab23782dc9b33004b5e6e9
wahedelfar <damietta4@gmail.com>
2026-09-25 10:10:34 +0300

    fix: allow generated Vercel API bundle import

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+// The API bundle is generated during the Vercel build and intentionally has no declaration file.
+// @ts-expect-error Generated JavaScript bundle.
 import { appRouter, createContext } from "../../dist/api-server.js";
 
 const app = express();
