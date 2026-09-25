808d22b95c50fde0fae4f53d96e365afca2521af
wahedelfar <damietta4@gmail.com>
2026-09-25 09:52:32 +0300

    build: bundle Vercel API server dependencies

---
+++ server/apiEntry.ts
@@ -0,0 +1,4 @@
+export { appRouter } from "./routers";
+export { createContext } from "./_core/context";
+export { registerOAuthRoutes } from "./_core/oauth";
+export { registerStorageProxy } from "./_core/storageProxy";
